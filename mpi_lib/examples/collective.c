789fec2ba8c507793e9d85c82453a0584ebcc789
LE Van Anh Khoa <106988219+AK087204@users.noreply.github.com>
2025-07-07 09:23:17 +0700

    Refactor MPI examples to remove easy wrapper and standardize context usage

--- mpi_lib/examples/collective.c
+++ mpi_lib/examples/collective.c
@@ -5,13 +5,13 @@
 #define ARRAY_SIZE 10
 
 int main(int argc, char *argv[]) {
-    mpi_easy_context_t ctx;
+    mpi_context_t ctx;
     
     // Initialize MPI
-    MPI_EASY_TRY(mpi_easy_init(argc, argv, &ctx));
+    MPI_TRY(mpi_init(argc, argv, &ctx));
     
-    int rank = mpi_easy_get_rank(&ctx);
-    int size = mpi_easy_get_size(&ctx);
+    int rank = mpi_get_rank(&ctx);
+    int size = mpi_get_size(&ctx);
     
     // Seed random number generator
     srand(time(NULL) + rank);
@@ -30,13 +30,13 @@ int main(int argc, char *argv[]) {
     
     // Broadcast from root
     int broadcast_value = 42;
-    if (mpi_easy_is_root(&ctx)) {
+    if (mpi_is_root(&ctx)) {
         printf("Root broadcasting value: %d\n", broadcast_value);
     }
     
-    MPI_EASY_TRY(mpi_easy_bcast(&broadcast_value, 1, MPI_EASY_INT, 0, ctx.comm));
+    MPI_TRY(mpi_bcast(&broadcast_value, 1, MPI_INT, 0, ctx.comm));
     
-    if (!mpi_easy_is_root(&ctx)) {
+    if (!mpi_is_root(&ctx)) {
         printf("Rank %d received broadcast value: %d\n", rank, broadcast_value);
     }
     
@@ -49,10 +49,10 @@ int main(int argc, char *argv[]) {
     
     // Reduce to find global sum
     int global_sum = 0;
-    MPI_EASY_TRY(mpi_easy_reduce(&local_sum, &global_sum, 1, MPI_EASY_INT, 
-                                 MPI_EASY_SUM, 0, ctx.comm));
+    MPI_TRY(mpi_reduce(&local_sum, &global_sum, 1, MPI_INT, 
+                       MPI_SUM, 0, ctx.comm));
     
-    if (mpi_easy_is_root(&ctx)) {
+    if (mpi_is_root(&ctx)) {
         printf("Global sum of all arrays: %d\n", global_sum);
         printf("Average per process: %.2f\n", (double)global_sum / size);
     }
@@ -66,17 +66,17 @@ int main(int argc, char *argv[]) {
     }
     
     int global_max = 0;
-    MPI_EASY_TRY(mpi_easy_allreduce(&local_max, &global_max, 1, MPI_EASY_INT, 
-                                    MPI_EASY_MAX, ctx.comm));
+    MPI_TRY(mpi_allreduce(&local_max, &global_max, 1, MPI_INT, 
+                          MPI_MAX, ctx.comm));
     
     printf("Rank %d: local max = %d, global max = %d\n", rank, local_max, global_max);
     
     // Calculate global average of each array position
     int global_averages[ARRAY_SIZE];
-    MPI_EASY_TRY(mpi_easy_allreduce(local_data, global_averages, ARRAY_SIZE, 
-                                    MPI_EASY_INT, MPI_EASY_SUM, ctx.comm));
+    MPI_TRY(mpi_allreduce(local_data, global_averages, ARRAY_SIZE, 
+                          MPI_INT, MPI_SUM, ctx.comm));
     
-    if (mpi_easy_is_root(&ctx)) {
+    if (mpi_is_root(&ctx)) {
         printf("Global averages by position: ");
         for (int i = 0; i < ARRAY_SIZE; i++) {
             printf("%.1f ", (double)global_averages[i] / size);
@@ -85,12 +85,12 @@ int main(int argc, char *argv[]) {
     }
     
     // Final synchronization
-    MPI_EASY_TRY(mpi_easy_barrier(ctx.comm));
+    MPI_TRY(mpi_barrier(ctx.comm));
     
-    if (mpi_easy_is_root(&ctx)) {
+    if (mpi_is_root(&ctx)) {
         printf("Collective communication example completed!\n");
     }
     
-    mpi_easy_finalize();
+    mpi_finalize();
     return 0;
 }
--- mpi_lib/examples/one_sided.c
+++ mpi_lib/examples/one_sided.c
@@ -3,32 +3,32 @@
 #define ARRAY_SIZE 10
 
 int main(int argc, char *argv[]) {
-    mpi_easy_context_t ctx;
+    mpi_context_t ctx;
     
     // Initialize MPI
-    MPI_EASY_TRY(mpi_easy_init(argc, argv, &ctx));
+    MPI_TRY(mpi_init(argc, argv, &ctx));
     
-    if (mpi_easy_get_size(&ctx) < 2) {
-        if (mpi_easy_is_root(&ctx)) {
+    if (mpi_get_size(&ctx) < 2) {
+        if (mpi_is_root(&ctx)) {
             printf("This example requires at least 2 processes\n");
         }
-        mpi_easy_finalize();
+        mpi_finalize();
         return 1;
     }
     
     // Allocate memory for shared array (only on root)
-    int *shared_array = mpi_easy_calloc(ARRAY_SIZE * sizeof(int), 0, mpi_easy_get_rank(&ctx));
+    int *shared_array = mpi_calloc(ARRAY_SIZE * sizeof(int), 0, mpi_get_rank(&ctx));
     
     // Create MPI window
-    mpi_easy_window_t win;
-    MPI_EASY_TRY(mpi_easy_win_create(shared_array, 
-                                     mpi_easy_is_root(&ctx) ? ARRAY_SIZE * sizeof(int) : 0,
-                                     sizeof(int), ctx.comm, &win));
+    mpi_window_t win;
+    MPI_TRY(mpi_win_create(shared_array, 
+                           mpi_is_root(&ctx) ? ARRAY_SIZE * sizeof(int) : 0,
+                           sizeof(int), ctx.comm, &win));
     
     // Lock window for passive target synchronization
-    MPI_EASY_TRY(mpi_easy_win_lock_all(&win));
+    MPI_TRY(mpi_win_lock_all(&win));
     
-    if (mpi_easy_is_root(&ctx)) {
+    if (mpi_is_root(&ctx)) {
         // Initialize array
         printf("Root initializing shared array...\n");
         for (int i = 0; i < ARRAY_SIZE; i++) {
@@ -42,23 +42,23 @@ int main(int argc, char *argv[]) {
     }
     
     // Synchronize
-    MPI_EASY_TRY(mpi_easy_barrier(ctx.comm));
+    MPI_TRY(mpi_barrier(ctx.comm));
     
-    if (!mpi_easy_is_root(&ctx)) {
+    if (!mpi_is_root(&ctx)) {
         // Non-root processes read and modify the array
-        int rank = mpi_easy_get_rank(&ctx);
+        int rank = mpi_get_rank(&ctx);
         int index = rank % ARRAY_SIZE;
         int old_value, new_value = rank * 100;
         
         // Read current value
-        MPI_EASY_TRY(mpi_easy_get(&old_value, 1, MPI_EASY_INT, 0, index * sizeof(int), &win));
+        MPI_TRY(mpi_get(&old_value, 1, MPI_INT, 0, index * sizeof(int), &win));
         printf("Rank %d read value %d from index %d\n", rank, old_value, index);
         
         // Compare and swap
         int compare_value = old_value;
         int result;
-        MPI_EASY_TRY(mpi_easy_compare_and_swap(&new_value, &compare_value, &result, 
-                                               MPI_EASY_INT, 0, index * sizeof(int), &win));
+        MPI_TRY(mpi_compare_and_swap(&new_value, &compare_value, &result, 
+                                     MPI_INT, 0, index * sizeof(int), &win));
         
         if (result == compare_value) {
             printf("Rank %d successfully updated index %d: %d -> %d\n", 
@@ -70,9 +70,9 @@ int main(int argc, char *argv[]) {
     }
     
     // Synchronize before final read
-    MPI_EASY_TRY(mpi_easy_barrier(ctx.comm));
+    MPI_TRY(mpi_barrier(ctx.comm));
     
-    if (mpi_easy_is_root(&ctx)) {
+    if (mpi_is_root(&ctx)) {
         printf("Final array: ");
         for (int i = 0; i < ARRAY_SIZE; i++) {
             printf("%d ", shared_array[i]);
@@ -81,14 +81,14 @@ int main(int argc, char *argv[]) {
     }
     
     // Clean up
-    MPI_EASY_TRY(mpi_easy_win_unlock_all(&win));
-    mpi_easy_win_destroy(&win);
-    mpi_easy_free(shared_array, 0, mpi_easy_get_rank(&ctx));
+    MPI_TRY(mpi_win_unlock_all(&win));
+    mpi_win_destroy(&win);
+    mpi_free(shared_array, 0, mpi_get_rank(&ctx));
     
-    if (mpi_easy_is_root(&ctx)) {
+    if (mpi_is_root(&ctx)) {
         printf("One-sided communication example completed!\n");
     }
     
-    mpi_easy_finalize();
+    mpi_finalize();
     return 0;
 }
