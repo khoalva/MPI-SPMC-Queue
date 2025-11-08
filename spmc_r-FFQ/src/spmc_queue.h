cf57e028becbdf21ca4710614f443d51d87d10a8
LE Van Anh Khoa <106988219+AK087204@users.noreply.github.com>
2025-11-08 22:42:52 +0700

    update for cluster test

--- spmc_r-FFQ/src/spmc_queue.h
+++ spmc_r-FFQ/src/spmc_queue.h
@@ -8,7 +8,7 @@
 #define EMPTY_CELL -1
 #define DEQUEUED_CELL -2
 #define MAX_QUEUE_SIZE 110000
-#define BATCH_SIZE 100
+#define BATCH_SIZE 10
 #define MAX_DEQUEUE_RETRIES 1
 #define MAX_WAIT_COUNT 1
 typedef struct {
